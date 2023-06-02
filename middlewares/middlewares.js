deea6182439e13eadf53e876d92f39efdb31af07
Guilherme <guilhermerbraga10@hotmail.com>
2023-06-01 23:52:32 -0300

    Implementação de token JWT e recodificação de codigo

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,25 +1,89 @@
-const UserModel = require('../models/usermodel');
-const database = require('../database/connect');
+const UserModel = require("../models/usermodel");
+const database = require("../database/connect");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function findUser(email) {
+  const db = await database.connectDatabase();
+  const userbd = await UserModel.findOne(email);
+  return userbd;
+}
 
 async function validateCreate(req, res, next) {
-  const {email, password } = req.body
+  const { email, password } = req.body;
 
   // Validando envio
-  if(!email){
-    return res.status(422).json({ msg: 'email é obrigatório' })
+  if (!email) {
+    return res.status(422).json({ msg: "email é obrigatório" });
   }
-  if(!password){
-    return res.status(422).json({ msg: 'Senha é obrigatório'})
+  if (!password) {
+    return res.status(422).json({ msg: "Senha é obrigatório" });
   }
 
   // Validando se ja existe usuario
-  const db = await database.connectDatabase();
-  const userExist = await UserModel.findOne({ email: email })
+  const userExist = await findUser({ email: email });
 
-  if(userExist){
-    return res.status(422).json({ msg: 'Email já em uso!'})
+  if (userExist) {
+    return res.status(422).json({ msg: "Email já em uso!" });
   }
   next();
 }
 
-module.exports = { validateCreate };
+async function authenticateUser(req, res, next) {
+  const { email, password } = req.body;
+  const user = await findUser({ email: email });
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!user) {
+    return res.status(404).json("Usuario não encontrado");
+  }
+  if (!checkPassword) {
+    return res.status(422).json("Senha incorreta");
+  }
+
+  next();
+}
+
+async function createToken(req, res, next) {
+  const { email } = req.body;
+  const user = await findUser({ email: email });
+  const secret = process.env.SECRET;
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    secret
+  );
+
+  res.token = token;
+  next();
+}
+
+async function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  const secret = process.env.SECRET;
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado" });
+  }
+
+  try {
+    const payload = jwt.verify(token, secret);
+    if(!payload){
+      return res.status(401).json({msg: 'INVALIDO'})
+    }
+  } catch (error) {
+    return res.status(401).json({msg: 'invalid token'})
+  }
+  
+  jwt.verify(token, secret);
+  next();
+}
+
+module.exports = {
+  validateCreate,
+  authenticateUser,
+  authenticateToken,
+  createToken,
+};
--- router/router.js
+++ router/router.js
@@ -1,88 +1,92 @@
-const { validateCreate } = require('../middlewares/middlewares');
-const bcrypt = require('bcrypt');
+const {
+  validateCreate,
+  authenticateUser,
+  authenticateToken,
+  createToken,
+} = require("../middlewares/middlewares");
+const bcrypt = require("bcrypt");
 
 module.exports = (app, repository) => {
+  app.get("/users", async (req, res) => {
+    const users = await repository.getUsers();
+    if (!users || !users.length) return res.sendStatus(404);
 
-    app.get('/users', async (req, res) => {
-        const users = await repository.getUsers();
-        if (!users || !users.length) return res.sendStatus(404);
+    res.json(users);
+  });
 
-        res.json(users);
-    })
+  app.get("/users/:id", authenticateToken, async (req, res) => {
+    const userId = req.params.id;
+    const user = await repository.getUsersId(userId);
+    if (!user) return res.sendStatus(404);
 
-    app.get('/users/:id', async (req, res) => {
-        const userId = req.params.id;
-        const user = await repository.getUsersId(userId);
-        if (!user) return res.sendStatus(404);
+    return res.status(200).json(user);
+  });
 
-        return res.status(200).json(user);
+  app.post(
+    "/users/authenticate",
+    authenticateUser,
+    createToken,
+    async (req, res) => {
+      const { email } = req.body;
+      const token = res.token;
+      const user = await repository.postValidateLogin(email);
+      return res.status(200).json({ msg: "autorizado", token });
+    }
+  );
+
+  app.post("/users", validateCreate, async (req, res) => {
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(req.body.password, salt);
+    const user = await repository.postCreateUser({
+      email: req.body.email,
+      password: passwordHash,
+      admin: req.body.admin,
     });
+    return res.status(200).json(user);
+  });
 
-    app.post('/users/authenticate', async (req, res ) => {
-        const { email, password } = req.body;
-        const user = await repository.postValidateLogin(email)
+  app.patch("/users/:id", async (req, res) => {
+    const userId = req.params.id;
+    const user = await repository.patchUpdateUser(userId, req.body);
+    return res.status(200).json(user);
+  });
 
-        if(!user)
-            return res.status(422).json('Usuario não encontrado')
+  app.delete("/users/:id", async (req, res) => {
+    const userId = req.params.id;
+    const user = await repository.deleteUser(userId);
+    return res.status(200).json(user);
+  });
 
-        if(password != user.password)
-            return res.status(422).json('Senha incorreta')
+  //consulta na tabela product
+  app.get("/product", async (req, res) => {
+    const product = await repository.getProduct();
+    if (!product || !product.length) return res.sendStatus(404);
 
-        return res.status(200).json('Autenticado')
-    })
+    res.json(product);
+  });
 
-    app.post('/users', validateCreate, async (req, res) => {
-        const salt = await bcrypt.genSalt(12);
-        const passwordHash = await bcrypt.hash(req.body.password, salt)
-        const user = await repository.postCreateUser({email: req.body.email, password: passwordHash, admin: req.body.admin });
-        return res.status(200).json(user);
-    });
+  app.get("/product/:id", async (req, res) => {
+    const productId = req.params.id;
+    const product = await repository.getProductById(productId);
+    if (!productId) return res.sendStatus(404);
 
-    app.patch('/users/:id', async (req, res) => {
-        const userId = req.params.id;
-        const user = await repository.patchUpdateUser(userId, req.body)
-        return res.status(200).json(user);
-    });
+    res.json(product);
+  });
 
-    app.delete('/users/:id', async (req, res) => {
-        const userId = req.params.id;
-        const user = await repository.deleteUser(userId)
-        return res.status(200).json(user);
-    });
+  app.post("/product", async (req, res) => {
+    const product = await repository.postCreateProduct(req.body);
+    return res.status(200).json(product);
+  });
 
+  app.patch("/product/:id", async (req, res) => {
+    const productId = req.params.id;
+    const product = await repository.patchUpdateProduto(productId, req.body);
+    return res.status(200).json(product);
+  });
 
-    //consulta na tabela product    
-    app.get('/product', async (req, res) => {
-        const product = await repository.getProduct();
-        if (!product || !product.length) return res.sendStatus(404);
-
-        res.json(product);
-    })
-
-    app.get('/product/:id', async (req, res) => {
-        const productId = req.params.id;
-        const product = await repository.getProductById(productId);
-        if (!productId) return res.sendStatus(404);
-
-        res.json(product)
-    })
-
-    app.post('/product', async (req, res) => {
-        const product = await repository.postCreateProduct(req.body);
-        return res.status(200).json(product);
-    })
-
-    app.patch('/product/:id', async (req, res) => {
-        const productId = req.params.id;
-        const product = await repository.patchUpdateProduto(productId, req.body)
-        return res.status(200).json(product);
-    })
-
-    app.delete('/product/:id', async (req, res) => {
-        const productId = req.params.id;
-        const product = await repository.deleteProduct(productId);
-        return res.status(200).json(product);
-    })
-
-}
-
+  app.delete("/product/:id", async (req, res) => {
+    const productId = req.params.id;
+    const product = await repository.deleteProduct(productId);
+    return res.status(200).json(product);
+  });
+};
